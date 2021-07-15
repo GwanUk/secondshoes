2ed859d0c7074433ff9c6109dcef3615cd88a845
GwanUk <guk1502012@gamil.com>
2021-07-15 19:20:12 +0900

    Initial commit

---
+++ src/main/java/com/market/secondshoes/SecondshoesApplication.java
@@ -0,0 +1,13 @@
+package com.market.secondshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondshoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondshoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/secondshoes/SecondshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.secondshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondshoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
