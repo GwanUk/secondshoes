0ebad089572ee05e11529838edfeb64ade21e0d1
GwanUk <guk1502012@gmail.com>
2021-08-10 14:07:03 +0900

    itemAddForm finished

--- src/main/java/com/market/secondshoes/dto/item/ItemAddDto.java
+++ src/main/java/com/market/secondshoes/dto/item/ItemAddDto.java
@@ -6,9 +6,12 @@ import com.market.secondshoes.domain.item.Gender;
 import com.market.secondshoes.domain.item.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
@@ -18,16 +21,21 @@ public class ItemAddDto {
     @NotBlank
     private String title;
 
+    @NotNull
     private Gender gender;
 
+    @NotNull
     private Size size;
 
+    @NotNull
     private Brand brand;
 
+    @NotNull
     private Category category;
 
     private Long price;
 
+    @Length(max = 10)
     private String explain;
 
     private List<MultipartFile> images;
